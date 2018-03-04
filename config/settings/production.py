e5d7914c51b95c12a39bc26ed06f82f8bb8b3500
Stephen Wagstaff <srwagsta@gmail.com>
2018-03-04 16:41:24 -0600

    Updated debug config for production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -44,7 +44,7 @@ from .base import *  # noqa
 # # Hosts/domain names that are valid for this site
 # # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [get_secret('DJANGO_ALLOWED_HOSTS')]
-DEBUG = get_secret('DEBUG')
+DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 # # END SITE CONFIGURATION
