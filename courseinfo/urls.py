03b086a331cb3d566503372f4184b7b3836e5964
Stephen Wagstaff <srwagsta@gmail.com>
2018-03-04 09:24:10 -0600

    Added all detail views, added the detail view URLS, started the final few detail templates

--- courseinfo/urls.py
+++ courseinfo/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .views import (InstructorList,
                     InstructorDetailView,
                     SectionList,
+                    SectionDetailView,
                     CourseList,
+                    CourseDetailView,
                     SemesterList,
+                    SemesterDetailView,
                     StudentList,
+                    StudentDetailView,
                     Courseinfo)
 
 app_name = 'courseinfo'
@@ -13,13 +17,21 @@ urlpatterns = [
 
     path('instructor/', InstructorList.as_view(), name='instructor_list'),
 
-    path('instructor/<pk>', InstructorDetailView.as_view(), name='instructor-detail'),
+    path('instructor/<pk>', InstructorDetailView.as_view(), name='instructor_detail'),
 
     path('section/', SectionList.as_view(), name='section_list'),
 
+    path('section/<pk>', SectionDetailView.as_view(), name='section_detail'),
+
     path('course/', CourseList.as_view(), name='course_list'),
+
+    path('course/<pk>', CourseDetailView.as_view(), name='course_detail'),
 
     path('semester/', SemesterList.as_view(), name='semester_list'),
 
+    path('semester/<pk>', SemesterDetailView.as_view(), name='semester_detail'),
+
     path('student/', StudentList.as_view(), name='student_list'),
+
+    path('student/<pk>', StudentDetailView.as_view(), name='student_detail'),
 ]
--- courseinfo/views.py
+++ courseinfo/views.py
@@ -20,10 +20,14 @@ class InstructorDetailView(DetailView):
         return context
 
 
-
 class SectionList(ListView):
     model = Section
     context_object_name = 'section_list'
+
+
+class SectionDetailView(DetailView):
+    model = Section
+    # I don't think I need to add any extra context, the default will do fine
 
 
 class CourseList(ListView):
@@ -31,11 +35,36 @@ class CourseList(ListView):
     context_object_name = 'course_list'
 
 
+class CourseDetailView(DetailView):
+    model = Course
+    # The default context should be fine
+
+
 class SemesterList(ListView):
     model = Semester
     context_object_name = 'semester_list'
 
 
+class SemesterDetailView(DetailView):
+    model = Semester
+
+    def get_context_data(self, **kwargs):
+        context = super(InstructorDetailView, self).get_context_data(**kwargs)
+        # context['section_list'] = Section.objects.all()
+        # We can add some more context here
+        return context
+
+
 class StudentList(ListView):
     model = Student
     context_object_name = 'student_list'
+
+
+class StudentDetailView(DetailView):
+    model = Student
+
+    def get_context_data(self, **kwargs):
+        context = super(InstructorDetailView, self).get_context_data(**kwargs)
+        # context['section_list'] = Section.objects.all()
+        # Add some context here
+        return context
